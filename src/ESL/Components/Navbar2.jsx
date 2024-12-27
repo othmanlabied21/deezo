e7be38d810c7b613328d20641726e6d8f3d6d563
Abdelmalek <crispybatata@gmail.com>
2024-12-27 21:48:09 +0100

    Menu added

--- src/ESL/Components/Navbar2.jsx
+++ src/ESL/Components/Navbar2.jsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import { React, useState } from 'react';
 import '../Styles/Navbar2.css';
 import { Link } from "react-router-dom";
 import Logo from "../Images/Logo.png";
 import Profile from "../Images/Profile.png";
+import Menu from './Menu';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
   return (
-  <nav className="navbar navbar-expand-lg">
-    <img className="navbar-brand logo" src={ Logo } alt="Esl ai Logo" />
-    <ul className="navbar-nav mx-auto">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">Accueil</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/services">Services</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/Equipes">Equipes</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/Projets">Projets</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/Testimonials">Testimonials</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/contact">Contact</Link>
-      </li>
-    </ul>
-    <div className="profileCont">
-      <img src={ Profile } alt="Profile" className='profile'/>
-    </div>
-  </nav>
+    <>
+      <nav className="navbar navbar-expand-lg">
+        <img className="navbar-brand logo" src={ Logo } alt="Esl ai Logo" />
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Accueil</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/services">Services</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Equipes">Equipes</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Projets">Projets</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Testimonials">Testimonials</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div className="profileCont" onClick={toggleMenu}>
+          <img src={ Profile } alt="Profile" className='profile'/>
+        </div>
+      </nav>
+      <Menu isOpen={isMenuOpen} closeMenu={closeMenu} />
+    </>
   );
 };
 
