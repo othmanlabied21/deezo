fca214bef785b30261b055dea8b78a22b83d3ae7
Abdelmalek <crispybatata@gmail.com>
2024-12-28 15:15:49 +0100

    Css modified

---
+++ src/ESL/Components/RequestlyTuto.jsx
@@ -0,0 +1,22 @@
+export default  RequestlyTuto = () => {
+    return (
+        <div className="requestly-container">
+            <div className="group-exp">
+                <img src="" alt="" />
+                <p></p>
+            </div>
+            <div className="group-exp">
+                <img src="" alt="" />
+                <p></p>
+            </div>
+            <div className="group-exp">
+                <img src="" alt="" />
+                <p></p>
+            </div>
+            <div className="group-exp">
+                <img src="" alt="" />
+                <p></p>
+            </div>
+        </div>
+    )
+}
