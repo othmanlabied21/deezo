2a9b0f84f0d975a2c93c64cbf96d6069bb09985e
othmanlb23 <othmanlabied21@gmail.com>
2025-01-10 17:33:29 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import React from "react";
-import ServicesSection from "./ESL/Components/ServicesSection";
 import "./App.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, BrowserRouter as Router, Navigate } from "react-router-dom";
+
+import Navbar2 from "./ESL/Components/Navbar2";
+import Footer from "./ESL/Components/Footer";
+import Section from "./ESL/Components/Section";
+import ServicesSection from "./ESL/Components/ServicesSection";
 import ContactForm from "./ESL/Components/ContactForm";
 import Testimonials from "./ESL/Components/Testimonials";
-import Footer from "./ESL/Components/Footer";
-import Navbar2 from "./ESL/Components/Navbar2";
-import Section from "./ESL/Components/Section";
 import Equipes from "./ESL/Components/Equipes";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";  // Use BrowserRouter here
-
 import ProjectManagement from "./ESL/Components/ProjectManagement";
 import Faq from "./ESL/Components/Faq";
 import BoiteMessage from "./ESL/Components/BoiteMessage";
 import Guide from "./ESL/Components/Guide";
 
 function App() {
-  const [activeComponent, setActiveComponent] = React.useState(null);
-
   return (
-    <Router> {/* Wrap the entire app with BrowserRouter */}
+    <Router>
       <div className="app">
         <Navbar2 />
         <main className="content">
-          <Section/>
           <Routes>
             <Route path="/" element={<Section />} />
-            <Route path="/services" element={<ServicesSection setActiveComponent={setActiveComponent} />} />
+            <Route path="/services" element={<ServicesSection />} />
             <Route path="/Equipes" element={<Equipes />} />
             <Route path="/contact" element={<ContactForm />} />
             <Route path="/Testimonials" element={<Testimonials />} />
@@ -34,6 +31,7 @@ function App() {
             <Route path="/FAQ" element={<Faq />} />
             <Route path="/BoiteMessage" element={<BoiteMessage />} />
             <Route path="/Guide" element={<Guide />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </main>
         <Footer />
